bfb576125f6854dab37a99cee8eebdec01fa16de
Rainy <850627734@qq.com>
2020-03-12 17:44:36 +0800

    gas-search-h5

---
+++ demo/gas-search-h5/rem.js
@@ -0,0 +1,16 @@
+(function () {
+    function rem () {
+        var html = document.getElementsByTagName('html')[0]
+        var size = 750
+        var deviceSize = window.screen.width || window.pageXOffset
+        var ratio = deviceSize / size
+
+        html.style.fontSize = ratio > 1
+            ? 100 + 'px'
+            : ratio < 0.3
+                ? 30 + 'px'
+                : ratio * 100 + 'px'
+    }
+    rem()
+    window.addEventListener('resize', rem)
+}())
